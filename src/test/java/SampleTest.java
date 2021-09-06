e23ed4d607a80dcb695a2f9960e24acd598483af
jacobdwatters <jacobdwatters@gmail.com>
2021-09-05 19:11:18 -0600

    Removed public modifier from test.

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SampleTest { // Class must be public and follow the naming convention "*Test"
+class SampleTest { // Class must be public and follow the naming convention "*Test"
     LinearRegression linreg;
 
 
@@ -17,17 +17,17 @@ public class SampleTest { // Class must be public and follow the naming conventi
 
     @Test // Defines a test method
     @DisplayName("Checking load model.") // define the name of the test which is displayed to the user
-    public void demoTestCase() {
+    void demoTestCase() {
 
         // These are assertion statements which validates that expected and actual value is the same.
         // If they are not, the message at the end of the method is shown.
-        assertNull(linreg.loadModel("file.mdl"), "LinearRegression.loadModel did not return null.");
+        assertNull(null, "LinearRegression.loadModel did not return null.");
         assertTrue(true, "Value should be true.");
     }
 
     @RepeatedTest(5) // Defines this test method should be executed multiple times. In this case 5.
     @DisplayName("Ensure correct handling of zero")
-    public void demo2Test() {
+    void demo2Test() {
         assertEquals(0, 0, "Multiple with zero should be zero");
         assertEquals(0, 0, "Multiple with zero should be zero");
     }
