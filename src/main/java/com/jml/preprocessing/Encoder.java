4bae9bd6fa7d32a32eafc8999dea4c74a0ee35f8
Jacob Watters <jacobdwatters@gmail.com>
2022-02-20 17:54:05 -0700

    Changed todo comment in Encode class.

--- src/main/java/com/jml/preprocessing/Encoder.java
+++ src/main/java/com/jml/preprocessing/Encoder.java
@@ -14,7 +14,8 @@ public class Encoder {
     // Hide default constructor.
     private Encoder() {throw new IllegalStateException("Cannot instantiate utility class.");}
 
-    // TODO: Add Encoding object that hold a map which shows what class became what value. That way the value can be decoded.
+    // TODO: Each encoder should be an object inheriting from the Encoder class. This will allow an encoder object to be
+    //  fit to the data so that any data can then be decoded.
 
 
     /**
